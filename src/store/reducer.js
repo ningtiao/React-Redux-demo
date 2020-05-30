4d43b5a3a968e8747a894084a1e38702cf363bd5
e <578755236@qq.com>
2020-05-30 21:47:27 +0800

    redux

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,16 +1,49 @@
+// const defaultState = {
+//     inputValue: '大白菜',
+//     list: [
+//         '早上起床',
+//         '中午吃饭',
+//         '晚上睡觉打豆豆'
+//     ]
+// }
+
+// export default (state = defaultState, action) => {
+//     console.log(state, action)
+//     // reducer里只能接受state, 不能改变state
+//     // 处理玩逻辑需要返回State
+//     if (action.type === 'changeInput') {
+//         let newState = JSON.parse(JSON.stringify(state))
+//         newState.inputValue = action.value
+//         return newState
+//     }
+
+//     if (action.type === 'addItem') {
+//         let newState = JSON.parse(JSON.stringify(state))
+//         newState.list.push(newState.inputValue)
+//         newState.inputValue = ''
+//         return newState
+//     }
+
+//     if (action.type === 'deleteItem') {
+//         let newState = JSON.parse(JSON.stringify(state))
+//         newState.list.splice(action.index, 1)
+//         return newState
+//     }
+
+//     return state
+// }
+
 const defaultState = {
     inputValue: '大白菜',
     list: [
         '早上起床',
         '中午吃饭',
-        '晚上睡觉打豆豆'
+        '晚上睡觉'
     ]
 }
 
 export default (state = defaultState, action) => {
     console.log(state, action)
-    // reducer里只能接受state, 不能改变state
-    // 处理玩逻辑需要返回State
     if (action.type === 'changeInput') {
         let newState = JSON.parse(JSON.stringify(state))
         newState.inputValue = action.value
@@ -19,16 +52,16 @@ export default (state = defaultState, action) => {
 
     if (action.type === 'addItem') {
         let newState = JSON.parse(JSON.stringify(state))
+        console.log(newState.inputValue)
         newState.list.push(newState.inputValue)
         newState.inputValue = ''
         return newState
     }
 
-    if (action.type === 'deleteItem') {
+    if (action.type === 'deleleItem') {
         let newState = JSON.parse(JSON.stringify(state))
         newState.list.splice(action.index, 1)
         return newState
     }
-
     return state
 }
--- src/views/index/index.js
+++ src/views/index/index.js
@@ -8,35 +8,33 @@ class Index extends Component {
     constructor(props) {
         super(props)
         this.state = store.getState()
-        this.changeInputValue = this.changeInputValue.bind(this)
+        this.inputChangeValue = this.inputChangeValue.bind(this)
+        this.addItem = this.addItem.bind(this)
         this.storeChange = this.storeChange.bind(this)
-        this.addList = this.addList.bind(this)
-        // this.delItem = this.delItem.bind(this)
         store.subscribe(this.storeChange)
     }
     render() {
         return (
             <SliderContainer>
                 <div className='app-container'>
-                    <div className="header-content">
-                        <Input 
-                            placeholder={this.state.inputValue}
-                            className="ant-form-input"
-                            onChange={this.changeInputValue}
-                            value={this.state.inputValue}
-                        />
-                        <Button type="primary" onClick={this.addList}>新增</Button>
+                    <div className='header-content'>
+                        <Input placeolder="请输入内容" onChange={this.inputChangeValue} className='ant-form-input'/>
+                        <Button type="primary" onClick={this.addItem}>新增</Button>
                     </div>
                     <div className="list-content">
-                        <List bordered
-                            dataSource={this.state.list}
-                            renderItem={(item, index) => (<List.Item clasname="list-item">
-                            <div className='flex-item'>
+                        <List 
+                        bordered
+                        dataSource={this.state.list}
+                        renderItem={(item, index) => (
+                            <List.Item classname="list-item">
+                             <div className='flex-item'>
                                 {item}
-                            </div>
-                            <div className='flex-item'>
-                                <Button type="primary" danger onClick={this.delItem.bind(this, index)}>删除</Button></div>
-                            </List.Item>)}
+                             </div>
+                             <div className='flex-item'>
+                                <Button type="primary" danger onClick={this.delItem.bind(this, index)}>删除</Button>
+                             </div>
+                            </List.Item>
+                          )}
                         />
                     </div>
                 </div>
@@ -44,23 +42,25 @@ class Index extends Component {
         )
     }
 
-    changeInputValue (e) {
+    inputChangeValue(e) {
+        console.log(e.target.value)
         const action = {
             type: 'changeInput',
             value: e.target.value
         }
         store.dispatch(action)
     }
-    addList () {
+
+    addItem () {
         const action = {
             type: 'addItem'
         }
         store.dispatch(action)
     }
+
     delItem (index) {
-        console.log(index)
         const action = {
-            type: 'deleteItem',
+            type: 'deleleItem',
             index
         }
         store.dispatch(action)
